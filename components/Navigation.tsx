d76b4223f1cfc24dd199ccf31b7c8b39bf7d1b3a
Kevin Kim <kevinlabx@gmail.com>
2025-12-15 22:12:31 -0600

    [Update] - Reduce some height of navbar -I felt like it was bit too chunky.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <div className="navbar bg-base-300 shadow-lg">
+    <div className="navbar bg-base-300 shadow-lg h-12 min-h-0 py-0">
       <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">
+        <Link href="/" className="btn btn-ghost btn-sm text-lg">
           📖 Bible Verse Game
         </Link>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal menu-sm px-1">
           <li>
             <Link href="/">Play</Link>
           </li>
